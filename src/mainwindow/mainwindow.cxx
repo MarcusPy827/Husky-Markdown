f5cca6bbfb5764df521cf719d2882f90a57465b6
MarcusPy827 <markus_verify@126.com>
2025-02-25 16:42:13 -0600

    Created: Project

---
+++ src/mainwindow/mainwindow.cxx
@@ -0,0 +1,10 @@
+#include "mainwindow.hxx"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
---
+++ src/mainwindow/mainwindow.hxx
@@ -0,0 +1,22 @@
+#ifndef MAINWINDOW_HXX
+#define MAINWINDOW_HXX
+
+#include "QMainWindow"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
+
+    private:
+        Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_HXX
