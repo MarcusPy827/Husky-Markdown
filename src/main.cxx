4efaf31da82de68aa80ada6d7f041719eb36b5d3
MarcusPy827 <markus_verify@126.com>
2025-03-12 14:36:17 -0500

    [src] Enabled: DevTools

--- src/main.cxx
+++ src/main.cxx
@@ -6,6 +6,13 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
+    /* 开发者工具 · 如需要开启请取消注释，number(<PORT>)中为端口号。要使用开发者工具，请在本机正常浏览器中访问localhost:<PORT>
+     * DevTools · Uncomment if devtools is needed. The port number is set on number(<PORT>).
+     * To use devtools, visit localhost:<PORT> on any browser.
+     */
+
+    qputenv("QTWEBENGINE_REMOTE_DEBUGGING", QByteArray::number(9000));
+
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
